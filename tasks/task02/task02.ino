d235f89e9fca0c19990237f82f5153fdf9e974d0
DerWeiseTeufel <76570054+DerWeiseTeufel@users.noreply.github.com>
2024-04-15 15:39:11 +0400

    Updated task02.ino

--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,73 +1,24 @@
-#include <TaskScheduler.h>
 
+#include <TaskManagerIO.h>
 
-// Инициализируем объект планировщика задач
-Scheduler scheduler;
-
-
-// Создаем таски для каждого из импульсов
-Task t1(10, TASK_FOREVER, [](){
-  digitalWrite(3, HIGH);
-  delayMicroseconds(5000);
-  digitalWrite(3, LOW);
-  delay(5000);
-});
-
-
-Task t2(1000, TASK_FOREVER, [](){
-  digitalWrite(5, HIGH);
-  delayMicroseconds(500);
-  digitalWrite(5, LOW);
-  delay(500);
-});
-
-
-Task t3(500, TASK_FOREVER, [](){
-  digitalWrite(6, HIGH);
-  delayMicroseconds(250);
-  digitalWrite(6, LOW);
-  delay(250);
-});
-
-
-Task t4(100, TASK_FOREVER, [](){
-  digitalWrite(9, HIGH);
-  delayMicroseconds(50);
-  digitalWrite(9, LOW);
-  delay(50);
-});
-
-
-Task t5(50, TASK_FOREVER, [](){
-  digitalWrite(10, HIGH);
-  delayMicroseconds(25);
-  digitalWrite(10, LOW);
-  delay(25);
-});
-
+// Определение констант пинов и периодов в микросекундах
+const int pins[] = {3, 5, 6, 9, 10};
+const unsigned long periods[] = {10000, 1000, 500, 100, 50}; // Периоды для каждого пина
+const int numPins = sizeof(pins) / sizeof(pins[0]); // Количество пинов
+int states[numPins] = {LOW}; // Начальные состояния пинов, инициализированы как LOW
 
 void setup() {
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-
-
-  // Добавляем созданные таски в планировщик
-  scheduler.addTask(t1);
-  scheduler.addTask(t2);
-  scheduler.addTask(t3);
-  scheduler.addTask(t4);
-  scheduler.addTask(t5);
-
-
-  // Задаем интервал обновления планировщика (1 мс)
-  scheduler.startPeriodic(1);
+  // Настройка направления каждого пина на OUTPUT
+  for (int i = 0; i < numPins; i++) {
+    pinMode(pins[i], OUTPUT);
+    // Запланировать переключение каждого пина с соответствующим периодом
+    taskManager.scheduleFixedRate(periods[i], [i] {
+      states[i] = !states[i]; // Переключение состояния
+      digitalWrite(pins[i], states[i]); // Установить новое состояние пина
+    }, TIME_MICROS);
+  }
 }
 
-
 void loop() {
-  // Запускаем планировщик задач
-  scheduler.execute();
+  taskManager.runLoop(); // Выполнение планировщика задач
 }
